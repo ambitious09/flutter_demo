c3bd842fc81c3ecebc2b2107763f99b65732c187
wyjq <liuhuihua@wyjq.com>
2019-12-12 18:45:56 +0800

    新建分支2

--- android/app/src/main/kotlin/demo/example/com/flutter_demo/MainActivity.kt
+++ android/app/src/main/kotlin/demo/example/com/flutter_demo/MainActivity.kt
@@ -23,7 +23,6 @@ class MainActivity : FlutterActivity() {
         Executors.newScheduledThreadPool(6)
         System.err.println("线程数量=" + Runtime.getRuntime().availableProcessors())
 //    AsyncTask
-        
         val intrnt = intent
         val action = intrnt?.action
         val type = intrnt?.type
